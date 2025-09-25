87cc13e4ba647fc3d6a61e598cf796eed943863c
Luka Obradović <lukaobradovic5@gmail.com>
2025-09-25 03:48:16 +0200

    pdf order summary generation

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Rezervacija;
 use Barryvdh\DomPDF\Facade\Pdf; 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 
@@ -9,9 +11,12 @@ class PDFController extends Controller
 {
     public function generatePDF()
     {
+        date_default_timezone_set('Europe/Berlin');
+
+        $orders=Rezervacija::all();
         $data = [
-            'title' => 'Laravel PDF Example',
-            'date' => date('m/d/Y'),
+            'title' => 'Spisak narudzbina - ' .date('d.m.Y H:i'),
+            'orders'=>$orders
         ];
 
         $pdf = PDF::loadView('pdf.orders', $data);
--- resources/views/pdf/orders.blade.php
+++ resources/views/pdf/orders.blade.php
@@ -3,9 +3,30 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>{{$title}}</title>
 </head>
 <body>
-    <h1>testiram</h1>
+    <h1>{{$title}}</h1>
+
+    @foreach($orders as $o)
+    <h3>{{ $o->user->name }} {{ $o->user->surname }}</h3>
+    <p>Datum: {{ $o->datum }}</p>
+    <p>Adresa: {{ $o->adresa }}</p>
+
+            <table>
+                <thead>
+                    <th>Naziv artikla</th>
+                    <th>Količina</th>
+                </thead>
+                <tbody>
+                    @foreach($o->stavkas as $s)
+                    <tr>
+                        <td>{{ $s->jelo->naziv_jela }}</td>
+                        <td>{{ $s->kolicina }}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+    @endforeach
 </body>
 </html>
